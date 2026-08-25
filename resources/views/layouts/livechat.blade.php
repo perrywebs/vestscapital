2305c39ca52b99b9a364663d156cf7b2bbac2fa1
stephen042 <makuochukwu042@gmail.com>
2026-08-25 17:21:15 +0100

    Add Smartsupp Live Chat integration to live chat layout

--- resources/views/layouts/livechat.blade.php
+++ resources/views/layouts/livechat.blade.php
@@ -1,4 +1,16 @@
 {{-- Add your livechat, telegram code here  --}}
+<!-- Smartsupp Live Chat script -->
+<script type="text/javascript">
+var _smartsupp = _smartsupp || {};
+_smartsupp.key = 'a59692d99ddb3b9a504d23ede3dd7ccbfdd84b74';
+window.smartsupp||(function(d) {
+  var s,c,o=smartsupp=function(){ o._.push(arguments)};o._=[];
+  s=d.getElementsByTagName('script')[0];c=d.createElement('script');
+  c.type='text/javascript';c.charset='utf-8';c.async=true;
+  c.src='https://www.smartsuppchat.com/loader.js?';s.parentNode.insertBefore(c,s);
+})(document);
+</script>
+<noscript>Powered by <a href="https://www.smartsupp.com" target="_blank">Smartsupp</a></noscript>
 
 {{--Start whatsapp code --}}
 
